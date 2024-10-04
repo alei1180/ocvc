cd3d07a4276ead217022c40d8c637e402f4a6524
Alexander Osadchy <alei1180@gmail.com>
2024-10-04 15:58:22 +0400

    feat(core): add new version notification in tray menu (#9)

--- src/ocvc/ocvc.py
+++ src/ocvc/ocvc.py
@@ -6,52 +6,82 @@ import keyboard
 import PIL.Image
 import pyperclip
 import pystray
+import requests
 from win32gui import GetForegroundWindow, GetWindowText
 
 
-def run(app_name: str) -> None:
+def run(app_name: str):
     logo = r"..\..\img\logo.png"
     app_image = PIL.Image.open(logo)
     app = pystray.Icon(
         app_name,
         app_image,
         title=app_name,
-        menu=pystray.Menu(
-            pystray.MenuItem(current_version(), on_clicked),
-            pystray.MenuItem("GitHub", on_clicked),
-            pystray.MenuItem("Close", on_clicked),
-        ),
+        menu=tray_menu(),
     )
 
     app.run(setup)
 
 
-def on_clicked(app_icon: pystray.Icon, item: pystray.MenuItem) -> None:
-    if str(item) == current_version():
+def tray_menu() -> list[pystray.MenuItem]:
+    menu_items = [pystray.MenuItem("GitHub", on_clicked),
+                  pystray.MenuItem(current_version(), on_clicked)]
+    if new_version_available():
+        menu_items.append(pystray.MenuItem("download new version",
+                                           on_clicked))
+    menu_items.append(pystray.MenuItem("Close", on_clicked))
+    return menu_items
+
+
+def new_version_available() -> bool:
+    cur_version = int_version_from_string(current_version())
+    last_version = github_ocvc_last_version_number()
+    return last_version > cur_version
+
+
+def int_version_from_string(string_version: str) -> int:
+    return int(string_version.replace("ver ", "").replace(".", ""))
+
+
+def current_version() -> str:
+    return "ver 1.3"
+
+
+def github_ocvc_last_version_number() -> int:
+    version = 0
+    response = requests.get("https://api.github.com/repos/alei1180/ocvc/releases/latest")
+    if response.status_code == 200:
+        tag = response.json()["name"]
+        version = int_version_from_string(tag)
+    return version
+
+
+def on_clicked(app_icon: pystray.Icon, item: pystray.MenuItem):
+    if str(item) == "GitHub":
+        webbrowser.open("https://github.com/alei1180/ocvc", new=2)
+    elif str(item) == current_version():
         tag = str(item).replace(" ", "")
         webbrowser.open(f"https://github.com/alei1180/ocvc/releases/tag/{tag}", new=2)
-    elif str(item) == "GitHub":
-        webbrowser.open("https://github.com/alei1180/ocvc", new=2)
+        print(github_ocvc_last_version_number())
+    elif str(item) == "download new version":
+        webbrowser.open("https://github.com/alei1180/ocvc/releases/latest/", new=2)
+        print(github_ocvc_last_version_number())
     elif str(item) == "Close":
         app_icon.visible = False
         app_icon.stop()
 
 
-def setup(tray_icon: pystray.Icon) -> None:
+def setup(tray_icon: pystray.Icon):
     tray_icon.visible = True
     if tray_icon.visible:
         wait_press_hotkey("Shift + Alt + V")
 
 
-def current_version():
-    return "ver 1.2"
-
-
-def wait_press_hotkey(hotkey: str) -> None:
+def wait_press_hotkey(hotkey: str):
     keyboard.add_hotkey(hotkey, lambda: open_code_in_vsc())
 
 
-def open_code_in_vsc() -> None:
+def open_code_in_vsc():
     current_window = GetWindowText(GetForegroundWindow())
     if not configurator_window(current_window, "Конфигуратор"):
         return None
@@ -81,7 +111,7 @@ def copy_clipboard() -> str:
     return copied_code
 
 
-def empty_clipboard() -> None:
+def empty_clipboard():
     if windll.user32.OpenClipboard(None):
         windll.user32.EmptyClipboard()
         windll.user32.CloseClipboard()
